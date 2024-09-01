b960dfc0f7b9f3ee60c8820a46dfbb77849542a2
iitz36IXTY <tharushawijekumara@gmail.com>
2024-09-01 22:59:35 +0530

    initialize commit

---
+++ ars-backend/src/test/java/com/backend/ars/ArsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
